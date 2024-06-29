0a865c18863d9898dd0a58a929703ad7c6d578e7
Saymon Shoab <saymon@Saymons-MacBook-Air.local>
2024-06-29 10:51:10 +0600

    update semester registration

--- src/app/modules/semesterRegistration/semesterRegistration.controller.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.controller.ts
@@ -66,13 +66,14 @@ const deleteByIdFromDB = catchAsync(async (req: Request, res: Response) => {
 
 const startMyRegistration = catchAsync(async (req: Request, res: Response) => {
     const user = (req as any).user;
-    const result = await SemesterRegistrationService.startMyRegistration(user.userId)
-    sendResponse(res, {
-        statusCode: httpStatus.OK,
-        success: true,
-        message: 'Student SemesterRegistration started successfully',
-        data: result
-    });
+    console.log(user)
+    // const result = await SemesterRegistrationService.startMyRegistration(user.userId)
+    // sendResponse(res, {
+    //     statusCode: httpStatus.OK,
+    //     success: true,
+    //     message: 'Student SemesterRegistration started successfully',
+    //     data: result
+    // });
 })
 export const SemesterRegistrationController = {
     insertIntoDB,
